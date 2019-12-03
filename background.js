d96aff024c0fa863f3aecd5ddbad9c4e6533b1bf
lellia <alessandro.lelli.external@ynap.com>
2019-12-03 18:38:38 +0100

    notification reminder

--- background.js
+++ background.js
@@ -1,3 +1,39 @@
-function isLastDay(dt) {
-  return new Date(dt.getTime() + 86400000).getDate() === 1;
+function isLastMonthDay(date) {
+  return new Date(date.getTime() + 86400000).getDate() === 1
+}
+
+function show() {
+  new Notification('Modisfill reminder', {
+    icon: 'icon.png',
+    body: 'Today is the last day of the month, have you already filled your WTC?'
+  });
+}
+
+if (!localStorage.isInitialized) {
+  localStorage.isActivated = true
+  localStorage.frequency = 1
+  localStorage.isInitialized = true
+}
+
+const today = new Date();
+
+if (window.Notification && !isLastMonthDay(today)) {
+
+  if (JSON.parse(localStorage.isActivated)) show()
+
+  let interval = 0
+
+  setInterval(() => {
+
+    interval++
+
+    if (
+      JSON.parse(localStorage.isActivated) &&
+      localStorage.frequency <= interval
+    ) {
+      show()
+      interval = 0
+    }
+
+  }, 150000)
 }
